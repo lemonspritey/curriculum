18b7c82768b1914c98222189bb036349872d8abc
lemonspritey <121148812+lemonspritey@users.noreply.github.com>
2022-12-24 00:59:06 +0900

    3-4訂正

--- PHPjob/3-4/answer.php
+++ PHPjob/3-4/answer.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
   <link rel="stylesheet" href="style.css">
 </head>
-<?php 
-$answers = [80, "HTML", "insert"];
-function checkAnswer($userResponse, $answer)
+<?php
+function checkAnswer($response, $answer)
 {
-    return ($userResponse == $answer) ? "正解！" : "残念・・・";
+  return ($response === $answer) ? "正解！" : "残念・・・";
 }
 ?>
-    <p><?php echo $_POST["name"]."さんの結果は・・・？"?></p>
-    <p>①の答え</p>
-    <p><?php echo(checkAnswer($_POST["answer0"], $answers[0]));?></p>
-    <p>②の答え</p>
-    <p><?php echo(checkAnswer($_POST["answer1"], $answers[1]));?></p>
-    <p>③の答え</p>
-    <p><?php echo(checkAnswer($_POST["answer2"], $answers[2]));?></p>
+<p><?php echo $_POST["name"] . "さんの結果は・・・？" ?></p>
+<p>①の答え</p>
+<p><?php echo (checkAnswer($_POST["response"][0], $_POST["answers"][0])); ?></p>
+<p>②の答え</p>
+<p><?php echo (checkAnswer($_POST["response"][1], $_POST["answers"][1])); ?></p>
+<p>③の答え</p>
+<p><?php echo (checkAnswer($_POST["response"][2], $_POST["answers"][2])); ?></p>
+
 </html>
--- PHPjob/3-4/question.php
+++ PHPjob/3-4/question.php
@@ -1,42 +1,51 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
   <link rel="stylesheet" href="style.css">
 </head>
-<form action = 'answer.php' method = 'post'>
+<form action='answer.php' method='post'>
   <?php
-    $name = $_POST["name"];
-    $options = array(array(80, 22, 20, 21),
-      array("PHP", "Python", "JAVA", "HTML"),
-      array("join", "select", "insert", "update"));
+  $name = $_POST["name"];
+  $options = array(
+    array("80", "22", "20", "21"),
+    array("PHP", "Python", "JAVA", "HTML"),
+    array("join", "select", "insert", "update")
+  );
   ?>
-  <input type="hidden" name="name" value="<?php echo $name?>"/>
+
+  <input type="hidden" name="name" value="<?php echo $name ?>" />
+  <?php
+  $answers = ["80", "HTML", "select"];
+  foreach ($answers as $value) {
+    echo '<input type="hidden" name="answers[]" value="' . $value . '"/>';
+  } ?>
   <?php printf("<p>お疲れ様です%sさん</p>", $name); ?>
 
-  
   <h2>①ネットワークのポート番号は何番？</h2>
 
-  <?php foreach($options[0] as $option) {?>
-      <input type="radio" name="answer0" value="<?php echo $option?>" />
-      <span> <?php echo $option;?></span>
-  <?php }?>
+  <?php foreach ($options[0] as $option) { ?>
+    <input type="radio" name="response[0]" value="<?php echo $option ?>" />
+    <span> <?php echo $option; ?></span>
+  <?php } ?>
 
   <h2>②Webページを作成するための言語は？</h2>
-  <?php foreach($options[1] as $option) {?>
-      <input type="radio" name="answer1" value="<?php echo $option?>" />
-      <span> <?php echo $option;?> </span>
-  <?php }?>
+  <?php foreach ($options[1] as $option) { ?>
+    <input type="radio" name="response[1]" value="<?php echo $option ?>" />
+    <span> <?php echo $option; ?> </span>
+  <?php } ?>
 
   <h2>③MySQLで情報を取得するためのコマンドは？</h2>
-  <?php foreach($options[2] as $option) {?>
-      <input type="radio" name="answer2" value="<?php echo $option?>" />
-      <span> <?php echo $option;?></span>
-  <?php }?>
+  <?php foreach ($options[2] as $option) { ?>
+    <input type="radio" name="response[2]" value="<?php echo $option ?>" />
+    <span> <?php echo $option; ?></span>
+  <?php } ?>
   <br>
   <input type="submit" value="回答する" />
 </form>
+
 </html>
